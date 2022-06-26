b44c28083b04dfb8c7841db19fd364e9228ae401
«Vlad» <vladislavrezgo@gmail.com>
2022-06-25 19:00:41 -0800

    переменная switch1

---
+++ z3_zadacha_3/Program.cs
@@ -0,0 +1,46 @@
+﻿int num = 0;
+Console.WriteLine ("введите число 1 до 7");
+num = Convert.ToInt32(Console.ReadLine());
+switch (num)   // свитч дает варианты
+{
+case 1:
+{
+    Console.WriteLine("Пн");
+    break;
+     }
+case 2:
+{
+    Console.WriteLine("Вт");
+    break;
+     }
+case 3:
+{
+    Console.WriteLine("Ср");
+    break;
+     }
+case 4:
+{
+    Console.WriteLine("Чт");
+    break;
+     }
+case 5:
+{
+    Console.WriteLine("Пт");
+    break;
+     }
+case 6:
+{
+    Console.WriteLine("Сб");
+    break;
+     }
+case 7:
+{
+    Console.WriteLine("Вс");
+    break;
+     }
+default:
+{
+    Console.WriteLine("ничего");
+    break;
+     }
+}
