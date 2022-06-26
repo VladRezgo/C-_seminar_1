6199b3f69abaed93ec5f3ea907a524e795d19bb7
«Vlad» <vladislavrezgo@gmail.com>
2022-06-25 19:24:17 -0800

    цикл дней недели

--- z3_zadacha_3/Program.cs
+++ z3_zadacha_3/Program.cs
@@ -1,46 +1,39 @@
 ﻿int num = 0;
-Console.WriteLine ("введите число 1 до 7");
-num = Convert.ToInt32(Console.ReadLine());
+
+while(num <=1 | num >=7)
+{
+    Console.WriteLine ("введите число 1 до 7");
+    num = Convert.ToInt32(Console.ReadLine());
+}
 switch (num)   // свитч дает варианты
 {
-case 1:
-{
-    Console.WriteLine("Пн");
-    break;
+    case 1:{
+        Console.WriteLine("Пн");
+        break;
      }
-case 2:
-{
-    Console.WriteLine("Вт");
-    break;
+    case 2:{
+        Console.WriteLine("Вт");
+        break;
      }
-case 3:
-{
-    Console.WriteLine("Ср");
-    break;
+    case 3:{
+        Console.WriteLine("Ср");
+        break;
      }
-case 4:
-{
-    Console.WriteLine("Чт");
-    break;
+    case 4:{
+        Console.WriteLine("Чт");
+        break;
      }
-case 5:
-{
-    Console.WriteLine("Пт");
-    break;
+    case 5:{
+        Console.WriteLine("Пт");
+        break;
      }
-case 6:
-{
-    Console.WriteLine("Сб");
-    break;
+    case 6:{
+        Console.WriteLine("Сб");
+        break;
      }
-case 7:
-{
-    Console.WriteLine("Вс");
-    break;
+    case 7:{
+        Console.WriteLine("Вс");
+        break;
      }
-default:
-{
-    Console.WriteLine("ничего");
-    break;
-     }
+
 }
