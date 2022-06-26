90cdbff394392d8734e7c9d68e99f99257016cd7
«Vlad» <vladislavrezgo@gmail.com>
2022-06-25 19:34:58 -0800

    показывает все числа от минусового до положительного

---
+++ z4/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.WriteLine("Введите положительное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int numnegative = num * -1;
+
+while (numnegative <= num) 
+{
+    Console.WriteLine(numnegative);
+    numnegative ++;
+}
