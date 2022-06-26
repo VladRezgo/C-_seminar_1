27884284aa8ea75d2eb0c1a75c172ee4540113cd
«Vlad» <vladislavrezgo@gmail.com>
2022-06-25 19:52:21 -0800

    показывает все числа от минусового до положительного

---
+++ z5/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трехразное число, а на выходе показывает последнее число
+Console.WriteLine("Введите трехзначное число");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n >= 100 && n <= 999)
+{
+Console.WriteLine(n%10);
+}
+else { Console.WriteLine("Error");
+}
